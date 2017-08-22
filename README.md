f856e7c868017f401a9f3902eb7488f6ff7d4891
Damien <damienebreaux@gmail.com>
2017-08-21 19:25:08 -0500

    update readme and cleaned up JS

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # liri-node-app
 a command line node app that takes in parameters and gives you back data from Twitter, Spotify, and OMDB APIs.
+
+before you begin please run `npm install` in your command line from the root folder of this project.
+
+Enter the following commands to receive a response:
+`node liri.js my-tweets` the most recent tweets from my twitter bot
+
+`node liri.js spotify-this-song '<song name here>'` This will display the song name, artist, album and a URL link to the song
+
+`node liri.js movie-this '<movie name here>` This will display the movie info on the movie you typed in
+
+`node liri.js do-what-it-says` this will read the random.txt file and run a command based on inputs from it
--- liri.js
+++ liri.js
@@ -1,11 +1,14 @@
+//setup node.js requests
 var request = require('request');
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 var fs = require("fs");
 
+//setting user input to var
 var searchItem = process.argv[3];
 
+//allows twitter to call tweets
 var client = new Twitter({
   consumer_key: 'NZecOzIXgDLnQcz3QO8wckula',
   consumer_secret: 'VA2GjVSsvidaKLWGRRCyZytnKsu75o4REJkM2UjC7P89oRFgP1',
@@ -34,14 +37,12 @@ switch (process.argv[2]) {
   case "do-what-it-says":
     doThisNode();
     break;
-
-    // default:
-    //     code block
 }
 
+//spotify function when calling "spotify-this-song"
 function spotifyNode() {
 
-  //if song search is empty
+  //if song search is empty, default to song
   if (!searchItem) {
     spotify.request("https://api.spotify.com/v1/tracks/3DYVWvPh3kGwPasp7yjahc")
       .then(function(data) {
@@ -64,14 +65,14 @@ function spotifyNode() {
         console.error('Error occurred: ' + err);
       });
   }
-  //if song search is not empty
+  //if song search is not empty search the song
   else {
     spotify.search({
       type: 'track',
       query: searchItem,
       limit: 1
     }, function(err, data) {
-      //return the results of the song serached.
+      //return the results of the song searched.
       console.log("----------------");
       console.log("");
       console.log("");
@@ -111,38 +112,59 @@ function twitterNode() {
 
 //movie search function
 function movieNode(){
+  var request = require('request');
   //api key 7fc00fef
-  // var API_KEY = "7fc00fef";
-  //
-  // request("http://www.omdbapi.com/?t=" + searchItem + "&y=&plot=short&apikey=" + API_KEY, function (error, response, body) {
-  //   // If the request is successful
-  //   if (!error && response.statusCode === 200) {
-  //
-  //     var movieInfo = JSON.parse(body);
-  //
-  //     console.log("Movie title: " + movieInfo.Title);
-  //     console.log("Year movie came out: " + movieInfo.Year);
-  //     console.log("IMDB Rating: " + movieInfo.imdbRating);
-  //     console.log("Actors in the movie: " + movieInfo.Actors);
-  //     console.log("Rotten tomatoes URL: " + movieInfo.imdbRating);
-  //   }
-  // });
+  //var API_KEY = "7fc00fef";
+  request("http://www.omdbapi.com/?t=" + searchItem + "apikey=7fc00fef", function (error, response, body) {
+
+    console.log("Movie title: " + JSON.parse(body).Title);
+    console.log("Year movie came out: " + JSON.parse(body).Year);
+    console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+    console.log("Actors in the movie: " + JSON.parse(body).Actors);
+    console.log("Rotten tomatoes URL: " + JSON.parse(body).imdbRating);
+  });
 }
 
-function doThisNode(){
+
+function doThisNode() {
 
   fs.readFile("random.txt", "utf8", function(err, data) {
-  if (err) {
-    return console.log(err);
-  }
+    if (err) {
+      return console.log(err);
+    } else {
+      // Break down all the itmes in the data array
+      data = data.split(", ");
+      var command = data[0];
+      var songName = data[1];
+      console.log(command + " " + songName);
 
-  // Break down all the numbers inside
-  data = data.split(", ");
-  var command = data[0];
-  var songName = data[1];
-    console.log(command + " " + songName);
+      //search using the data on random.txt doc
+      spotify.search({
+        type: 'track',
+        query: songName,
+        limit: 1
+      }, function(err, data) {
+        //return the results of the song serached.
+        console.log("----------------");
+        console.log("");
+        console.log("");
+        //writes album
+        console.log('Artist: ' + data.tracks.items[0].album.artists[0].name);
+        //writes song
+        console.log('Song Name: ' + data.tracks.items[0].name);
+        //writes link
+        console.log('Preview Link: ' + data.tracks.items[0].preview_url);
+        //writes album
+        console.log('Album Name: ' + data.tracks.items[0].album.name);
+        console.log("");
+        console.log("");
+        console.log("----------------");
 
-    spotifyNode(command,query.songName);
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
 
-});
+      });
+    }
+  });
 }
